2f864251c12f48e4ef109152468efde2a84359d7
Leonardo Rivera <leorivera_88@hotmail.com>
2024-10-31 15:09:19 -0400

    disable logs

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import { pinoHttp } from 'pino-http';
 
+const ignoreUrls = ['/api-docs', '/health'];
+
 export const requestLogger = pinoHttp({
 	autoLogging: {
 		ignore(req) {
-			// disable Swagger UI logs
-			return req.url?.startsWith('/api-docs') ?? false;
+			// disable Swagger UI logs and health endpoints
+			return ignoreUrls.some(url => req.url?.startsWith(url)) ?? false;
 		},
 	},
 	serializers: {
