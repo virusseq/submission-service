482f2222e68f21981a44698c6195e1faed5be615
Leonardo Rivera <leorivera_88@hotmail.com>
2025-07-15 11:08:32 -0400

    override get submitted data controller

---
+++ src/common/validation.ts/common.ts
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import type { ParsedQs } from 'qs';
+import { z } from 'zod';
+
+import { VIEW_TYPE } from '@overture-stack/lyric';
+
+import { lyricProvider } from '@/core/provider.js';
+
+export const entityNameSchema = z.string().trim().min(1);
+
+export interface paginationQueryParams extends ParsedQs {
+	page?: string;
+	pageSize?: string;
+}
+
+export const positiveInteger = z.string().superRefine((value, ctx) => {
+	const parsed = parseInt(value);
+	if (isNaN(parsed)) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.invalid_type,
+			expected: 'number',
+			received: 'nan',
+		});
+	}
+
+	if (parsed < 1) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.too_small,
+			minimum: 1,
+			inclusive: true,
+			type: 'number',
+		});
+	}
+});
+
+export const pageSizeSchema = z.string().superRefine((value, ctx) => {
+	const parsed = parseInt(value);
+	if (isNaN(parsed)) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.invalid_type,
+			expected: 'number',
+			received: 'nan',
+		});
+	}
+
+	if (parsed < 1) {
+		ctx.addIssue({
+			code: z.ZodIssueCode.too_small,
+			minimum: 1,
+			inclusive: true,
+			type: 'number',
+		});
+	}
+});
+
+export const paginationQuerySchema = z.object({
+	page: positiveInteger.optional(),
+	pageSize: pageSizeSchema.optional(),
+});
+
+export type SQON = NonNullable<ReturnType<typeof lyricProvider.utils.convertSqonToQuery.parseSQON>>;
+
+export const sqonSchema = z.custom<SQON>((value) => {
+	try {
+		lyricProvider.utils.convertSqonToQuery.parseSQON(value);
+		return true;
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (error) {
+		return false;
+	}
+}, 'Invalid SQON format!');
+
+export const viewSchema = z.string().toLowerCase().trim().min(1).pipe(VIEW_TYPE);
---
+++ src/controllers/submitted-data/getSubmittedDataByQuery.ts
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2025 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { type Response } from 'express';
+import type { ParamsDictionary } from 'express-serve-static-core';
+import { z } from 'zod';
+
+import {
+	convertToViewType,
+	type PaginationMetadata,
+	type SubmittedDataResponse,
+	VIEW_TYPE,
+} from '@overture-stack/lyric';
+
+import logger from '@/common/logger.js';
+import {
+	entityNameSchema,
+	type paginationQueryParams,
+	paginationQuerySchema,
+	sqonSchema,
+	viewSchema,
+} from '@/common/validation.ts/common.js';
+import { lyricProvider } from '@/core/provider.js';
+import { type RequestValidation, validateRequest } from '@/middleware/requestValidation.js';
+import type { SubmissionManifest } from '@/submission/submitRequest.js';
+import { asArray } from '@/utils/format.js';
+
+export interface dataQueryParams extends paginationQueryParams {
+	entityName?: string | string[];
+	view?: string;
+}
+
+interface getDataPathParams extends ParamsDictionary {
+	categoryId: string;
+	organization: string;
+}
+
+const RequestSchema: RequestValidation<object, dataQueryParams, getDataPathParams> = {
+	body: sqonSchema,
+	query: z
+		.object({
+			entityName: z.union([entityNameSchema, entityNameSchema.array()]).optional(),
+			view: viewSchema.optional(),
+		})
+		.merge(paginationQuerySchema)
+		.superRefine((data, ctx) => {
+			if (data.view === VIEW_TYPE.Values.compound && data.entityName && data.entityName?.length > 0) {
+				ctx.addIssue({
+					code: z.ZodIssueCode.custom,
+					message: 'is incompatible with `compound` view',
+					path: ['entityName'],
+				});
+			}
+		}),
+	pathParams: z.object({
+		categoryId: z.string(),
+		organization: z.string(),
+	}),
+};
+
+type SubmittedDataWithFilesResponse = SubmittedDataResponse & {
+	files?: SubmissionManifest[];
+};
+
+type SubmittedDataPaginatedWithFilesResponse = {
+	pagination: PaginationMetadata;
+	records: SubmittedDataWithFilesResponse[];
+};
+
+// Default values for this endpoint
+const defaultPage = 1;
+const defaultPageSize = 20;
+const defaultView = VIEW_TYPE.Values.flat;
+
+export const byQuery = validateRequest(
+	RequestSchema,
+	async (req, res: Response<SubmittedDataPaginatedWithFilesResponse>, next) => {
+		try {
+			const categoryId = Number(req.params.categoryId);
+			const organization = req.params.organization;
+			const sqon = lyricProvider.utils.convertSqonToQuery.parseSQON(req.body);
+
+			// query params
+			const entityName = asArray(req.query.entityName || []);
+			const page = parseInt(String(req.query.page)) || defaultPage;
+			const pageSize = parseInt(String(req.query.pageSize)) || defaultPageSize;
+			const view = convertToViewType(req.query.view) || defaultView;
+
+			logger.info(
+				`Request Submitted Data on categoryId '${categoryId}' and organization '${organization}'`,
+				`pagination params: page '${page}' pageSize '${pageSize}'`,
+				`view '${view}'`,
+			);
+
+			const submittedDataResult = await lyricProvider.services.submittedData.getSubmittedDataByOrganization(
+				categoryId,
+				organization,
+				{ page, pageSize },
+				{ entityName, view, sqon },
+			);
+
+			if (submittedDataResult.metadata.errorMessage) {
+				throw new lyricProvider.utils.errors.NotFound(submittedDataResult.metadata.errorMessage);
+			}
+
+			// TODO: include files in this response
+			const response: SubmittedDataPaginatedWithFilesResponse = {
+				pagination: {
+					currentPage: page,
+					pageSize: pageSize,
+					totalPages: Math.ceil(submittedDataResult.metadata.totalRecords / pageSize),
+					totalRecords: submittedDataResult.metadata.totalRecords,
+				},
+				records: submittedDataResult.result,
+			};
+
+			return res.status(200).send(response);
+		} catch (error) {
+			next(error);
+		}
+	},
+);
